7ba51729cfbc6f6fd0983f34fef20368d81b3de2
Endzhe Galieva <endzhe91@gmail.com>
2021-03-09 12:08:18 +0300

    added input

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,12 +1,9 @@
 import React from 'react'
+import Post from './Post'
 
 export default ({ posts }) => {
   if (!posts.length) {
     return <button className="btn btn-primary">Загрузить</button>
   }
-  return (
-    <div>
-      <h1>Fetched Posts</h1>
-    </div>
-  )
+  return posts.map(post => <Post post={post} key={post} />)
 }
