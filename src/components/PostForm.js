cd690cb02d2accafa4b7cd316bd6ab6c70f49ce4
Endzhe Galieva <endzhe91@gmail.com>
2021-03-09 17:51:20 +0300

    added posts

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,14 +1,35 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {createPost} from "./redux/actions";
 
-export default class PostForm extends React.Component {
+class PostForm extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      title: ''
+    }
   }
 
   submitHandler = event => {
     event.preventDefault()
+
+    const {title} = this.state
+
+    const newPost = {
+     title, id: Date.now().toString()
+    }
+
+    this.props.createPost(newPost)
+    this.setState({ title: '' })
+
+  }
+
+  changeInputHandler = event => {
+    event.persist()
+    this.setState( prev => ({...prev, ...{
+      [event.target.name]: event.target.value
+    }}))
   }
 
   render() {
@@ -16,10 +37,24 @@ export default class PostForm extends React.Component {
       <form onSubmit={this.submitHandler}>
         <div className="form-group">
           <label htmlFor="title">Заголовок поста</label>
-          <input type="text" className="form-control" id="title" />
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={this.state.title}
+            name="title"
+            onChange={this.changeInputHandler}
+          />
         </div>
         <button className="btn btn-success" type="submit">Создать</button>
       </form>
     )
   }
 }
+
+const mapDispatchToProps = {
+  createPost
+}
+
+
+export default connect(null, mapDispatchToProps)(PostForm)
